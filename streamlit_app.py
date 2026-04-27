3852cd9fc52a65c4a0ee4464965647ab3608dba2
Nancy398 <nancy@moohousing.com>
2026-04-27 16:24:23 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -141,6 +141,6 @@ def profit(row):
         return rev * 0.98 - fixed
     else:
         return 0
-final_df['profit'] = final_df.apply(profit)
+final_df['profit'] = final_df.apply(profit,axis=1)
     
 st.dataframe(final_df)
