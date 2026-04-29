51f5138a267a2404137ab8f45af26ae3c44e19b2
Nancy398 <nancy@moohousing.com>
2026-04-29 15:58:49 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -317,12 +317,12 @@ with tab_apartments:
             
         # 4. 去掉全为空的行
         df = df.dropna(how='all').reset_index(drop=True)
-        
         return df
     
     df_2025 = read_file("Apartment Referral List","2025"header_row=1)
     df_expense = read_file("Apartments FA","Expense")
     st.dataframe(df_expense)
+    st.dataframe(df_2025)
     df_2025['Received Commission'] = (
         df_2025['Received Commission']
         .astype(str)
