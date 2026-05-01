d7861bb490bf96568fd3e6cd9d9e3096b24c6e5f
Nancy398 <nancy@moohousing.com>
2026-05-01 10:40:25 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -411,7 +411,6 @@ with tab_apartments:
     
     # A. 加载原始数据
     df_2025 = read_file("Apartment Referral List", "2025", header_row=1)
-    st.dataframe(df_2025)
     df_2026 = read_file("Apartment Referral List", "2026", header_row=1)
     df_expense_raw = read_file("Apartments FA", "Expense")
     
